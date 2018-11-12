161445e9321241147fe086b28c319522067f53e1
Joshua Conero <brximl@163.com>
2018-11-12 15:33:37 +0800

    添加测试目录

---
+++ example/base/target/debug/base.d
@@ -0,0 +1,1 @@
+D:\Joshua\gits-suroooog\conero\uymas-rs\master\example\base\target\debug\base.pdb: D:\Joshua\gits-suroooog\conero\uymas-rs\master\example\base\src\main.rs
---
+++ example/base/target/debug/deps/base-0cf7b256b91a0326.d
@@ -0,0 +1,5 @@
+D:\Joshua\gits-suroooog\conero\uymas-rs\master\example\base\target\debug\deps\base-0cf7b256b91a0326.exe: src\main.rs
+
+D:\Joshua\gits-suroooog\conero\uymas-rs\master\example\base\target\debug\deps\base-0cf7b256b91a0326.d: src\main.rs
+
+src\main.rs:
