061f70c0a3b2e90c027103aaea84cb45c82d73e2
Joshua Conero <brximl@163.com>
2018-12-12 22:40:06 +0800

    尝试编写代码

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,7 @@
 // decimal 十进制处理
+// 数字格式
+pub const NUMBER_STR: &'static str = "0123456789abcdefghijklmnopqrstuvzxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-=";
+pub const NSTR_LEN: usize = NUMBER_STR.len();
 
 
 pub struct Decimal{
@@ -12,6 +15,10 @@ impl Decimal{
     }
     // 数据转换
     pub fn to_n(&self, base: i32) -> String{
+        // 限制最大长度
+        if base >= NSTR_LEN{
+            return String::from("");
+        }
         let mut num = self.num;
         let mut bque: Vec<i32> = vec![];
         let mut value: String;
@@ -25,13 +32,22 @@ impl Decimal{
             num = (num - md)/base;
         }
         // 数据收集
-        let mut sque = vec![];
+        //let mut sque: Vec<str> = vec![];
+        let mut sque:Vec<str> = Vec::new();
         let mut i = (bque.len() - 1) as i32;
+        let mut ref_que:&mut Vec<u8>;
+        // 非安全代码
+        unsafe {
+            ref_que = String::from(NUMBER_STR).as_mut_vec();
+        }
+
         while i > -1{
-            sque.push(bque[i]);
+            let index = bque[i as usize];
+            sque.push(ref_que[index]);
             i -= 1;
         }
-        value
+        //value = sque.join(&String::from(""))
+        sque.join("")
     }
 }
 
