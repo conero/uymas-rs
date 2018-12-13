9cc37cd8782fd216d0f8dacbebe21d77ff55d3c9
Joshua Conero <brximl@163.com>
2018-12-13 23:49:12 +0800

    Decimal::to_n 编译通过

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,7 +1,6 @@
 // decimal 十进制处理
 // 数字格式
 pub const NUMBER_STR: &'static str = "0123456789abcdefghijklmnopqrstuvzxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-=";
-pub const NSTR_LEN: usize = NUMBER_STR.len();
 
 
 pub struct Decimal{
@@ -16,12 +15,13 @@ impl Decimal{
     // 数据转换
     pub fn to_n(&self, base: i32) -> String{
         // 限制最大长度
-        if base >= NSTR_LEN{
+        let max_strlen = NUMBER_STR.len() as i32;
+        if base >= max_strlen{
             return String::from("");
         }
         let mut num = self.num;
         let mut bque: Vec<i32> = vec![];
-        let mut value: String;
+        //let mut value: String;
         loop {
             if num < base{
                 bque.push(num);
@@ -32,18 +32,18 @@ impl Decimal{
             num = (num - md)/base;
         }
         // 数据收集
-        let mut sque: Vec<str> = vec![];
+        let mut sque: Vec<String> = vec![];
         //let mut sque:Vec<str> = Vec::new();
         let mut i = (bque.len() - 1) as i32;
-        let mut ref_que:&mut Vec<u8>;
-        // 非安全代码
-        unsafe {
-            ref_que = String::from(NUMBER_STR).as_mut_vec();
-        }
+
+        let ref_str = NUMBER_STR.get(..(base as usize));
+        //let ref_que = format!("{}", ref_str).split("");
+        //let ref_que = ref_str.expect(&"").split("");
+        let ref_que:Vec<&str> = ref_str.unwrap().split("").collect();
 
         while i > -1{
-            let index = bque[i as usize];
-            sque.push(ref_que[index]);
+            let index = (bque[i as usize]) as usize;
+            sque.push(ref_que[index].to_string());
             i -= 1;
         }
         //value = sque.join(&String::from(""))
@@ -57,6 +57,8 @@ mod test{
 
     #[test]
     fn new(){
-        assert_eq!("", Decimal::new(100).to_n(10))
+        //assert_eq!("1100011", Decimal::new(99).to_n(2));
+        //assert_eq!("143", Decimal::new(99).to_n(8));
+        assert_eq!("63", Decimal::new(99).to_n(16));
     }
 }
