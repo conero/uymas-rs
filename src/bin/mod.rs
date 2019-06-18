ad35542ac13700e356b685bac1f4856000ede7cd
Conero <conero@163.com>
2019-06-18 16:28:14 +0800

    bin 内部路由器搭建基本的骨架

--- src/bin/mod.rs
+++ src/bin/mod.rs
@@ -1,3 +1,29 @@
 //bin 命令程序包
 //2019年6月18日 星期二
 //Joshua Conero
+
+/// `Router` 的基本使用
+///
+/// # Example
+///
+/// ```rust
+/// use uymas::bin::router::Router;
+/// let app = Router::new();
+/// app.register("app");
+/// app.run();      // 运行命运
+/// ```
+// 模块列表
+mod router;
+
+// 项目测试
+#[cfg(test)]
+mod router_test {
+    use crate::bin::router::Router;
+
+    #[test]
+    fn base_new() {
+        let app = Router::new();
+        app.run();
+        assert_eq!(1, 2)
+    }
+}
