8c8dd216ed71bdd2b62ce466d0176ef761dc4794
Joshua Conero <conero@163.com>
2022-10-31 11:11:16 +0800

    merge: 合并后的代码处理

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-// 版本信息
-// 项目版本信息
-mod uymas {
-    const VERSION: &str  = "0.1.0-alpha";
-    const RELEASE: &str = "2019-06-18";
-    const SINCE: &str = "2018-11-12";
-    const CODE: &str = "Uymas";
-    const TITLE: &str = "rust 语言工具包";
-}
