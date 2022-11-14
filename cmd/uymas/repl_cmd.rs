683f9c9b34c07e0f5961816d3481e2f0cc10eca9
Joshua Conero <conero@163.com>
2022-11-14 17:44:37 +0800

    pref(uymas): repl 输入格式化提示优化

--- cmd/uymas/repl_cmd.rs
+++ cmd/uymas/repl_cmd.rs
@@ -1,6 +1,7 @@
 // 注入式命令
 
 use cli::args::Args;
+use std::io::Write;
 
 pub struct ReplCmd {
     arg: Option<Args>,
@@ -12,16 +13,19 @@ impl ReplCmd {
     }
 
     // 执行
+    //参考: [Rust print消息不换行的方法](https://blog.csdn.net/hustlei/article/details/102511654)
     pub fn run(&mut self, arg: &Args) {
         self.arg = Some(arg.clone());
         println!("Repl 应用 :)- ");
         loop {
-            let mut input_string = String::new();
             print!("$> ");
+            std::io::stdout().flush().expect("Fail");
+            let mut input_string = String::new();
             match std::io::stdin().read_line(&mut input_string) {
                 Ok(_) => {
+                    input_string = input_string.trim().to_string();
                     if input_string == "exit" {
-                        println!("Bye, Have a good day!");
+                        println!("Bye, Have a good day! Ha.");
                         break;
                     }
                     println!("{}", input_string);
