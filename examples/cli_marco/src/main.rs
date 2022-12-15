c868ffd43737a8376e699d6667524c04fe1949b0
Joshua Conero <conero@163.com>
2022-12-15 23:55:22 +0800

    pref(cli_derive): 初步实现-cli_command-derive

--- examples/cli_marco/src/main.rs
+++ examples/cli_marco/src/main.rs
@@ -1,11 +1,21 @@
 use cli::cmd::{Cmd, CmdRunOs};
-use cli::derive::CliApp;
+use cli::derive::{cli_command, CliApp};
 use cli::CliApp;
 
 #[derive(CliApp)]
 struct MarcoApp {}
 
+impl MarcoApp {
+    #[cli_command(Help, "help")]
+    pub fn help(&self) {
+        println!("help   命令");
+        // 测试代码
+    }
+}
+
 fn main() {
     let app = MarcoApp {};
     app.run();
+    // 指定 help
+    app.help();
 }
